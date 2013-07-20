e457ac041e0ba90847dcd4f6698291f50041daa1
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2013-07-21 00:38:29 +0100

    Connect some clients and test the first scenario

--- spec/lib/follower_maze/server_spec.rb
+++ spec/lib/follower_maze/server_spec.rb
@@ -1,13 +1,39 @@
 require 'spec_helper'
+require 'timeout'
 
 describe FollowerMaze::Server do
 
+  let(:event_client)  { TCPSocket.new 'localhost', 9090 }
+  let(:client_1)      { TCPSocket.new 'localhost', 9099 }
+  let(:client_2)      { TCPSocket.new 'localhost', 9099 }
+
   before do
     Thread.new { subject.start }
-    client = TCPSocket.new 'localhost', 9099
+    client_1.write "60\r\n"
+    client_2.write "50\r\n"
+    sleep 4
   end
 
-  context 'Follow: Only the To User Id should be notified'
+  after do
+    client_1.close
+    client_2.close
+    event_client.close
+  end
+
+  context 'Follow: Only the To User Id should be notified' do
+    let(:message) { "666|F|60|50\r\n" }
+
+    before { event_client.write message }
+
+    it 'receives a message at client 2' do
+      Timeout.timeout(5) { client_2.readpartial(1024) }.first.should eql message
+    end
+
+    it 'does not receive message at client 1' do
+      expect { Timeout.timeout(5) { client_1.readpartial(1024) } }.to raise_error Timeout::Error
+    end
+  end
+
   context 'Unfollow: No clients should be notified'
   context 'Broadcast: All connected user clients should be notified'
   context 'Private Message: Only the To User Id should be notified'
