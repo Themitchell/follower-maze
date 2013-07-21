2be8def0bba9fe3ad7ea17180cf69ab6d78261ad
Andrew Mitchell <andrew.mitchell@unboxedconsulting.com>
2013-07-21 16:14:36 +0100

    #add_follower method for user
    Only store the id rather than the whole object

--- lib/follower_maze/user.rb
+++ lib/follower_maze/user.rb
@@ -1,15 +1,27 @@
+require './lib/follower_maze/user_store'
+
 module FollowerMaze
   class User
 
-    attr_reader :id, :connection
+    attr_reader :id, :connection, :follower_ids
 
     def initialize id, connection
       @id = id.to_i
       @connection = connection
+      @follower_ids = []
     end
 
     def write *args
       connection.write *args
+    end
+
+    def followers
+      Logger.info @follower_ids.inspect
+      @follower_ids.map { |id| UserStore.find id }
+    end
+
+    def add_follower user
+      @follower_ids << user.id
     end
 
   end
--- spec/lib/follower_maze/user_spec.rb
+++ spec/lib/follower_maze/user_spec.rb
@@ -7,4 +7,18 @@ describe FollowerMaze::User do
 
   its(:id)          { should eql 1 }
   its(:connection)  { should eql connection }
+  its(:followers)   { should eql [] }
+
+  describe '#add_follower' do
+    let(:user) { FollowerMaze::User.new 2, connection }
+    before do
+      FollowerMaze::UserStore.should_receive(:find).with(2).once { user }
+      subject.add_follower user
+    end
+
+    it 'adds a user to the list of followers_id' do
+      subject.followers.should include user
+    end
+  end
+
 end
